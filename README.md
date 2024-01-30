d3ed7082bc0b11ece89c751cdf84a031cbddedea
jmnote <opcore@gmail.com>
2024-01-30 08:00:25 +0000

    reflection

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # go-grpc-greeter-server
+
--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// https://github.com/grpc/grpc-go/blob/v1.61.0/examples/features/reflection/server/main.go
 /*
  *
  * Copyright 2019 gRPC authors.
@@ -41,7 +42,7 @@ type hwServer struct {
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *hwServer) SayHello(_ context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
+func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
@@ -49,7 +50,7 @@ type ecServer struct {
 	ecpb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(_ context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
+func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
